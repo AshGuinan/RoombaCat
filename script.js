8ec4dfc0852cd91f22cefb88e94b997fe730e214
Aisling <a.guinan3@nuigalway.ie>
2016-11-24 16:44:12 +0000

    Audio controls, battery sprite and credits added

--- script.js
+++ script.js
@@ -15,17 +15,18 @@ var canvas = document.getElementById('canvas'),
     numberOfFrames = 3,
     startTime,
     currentTime,
-    total,
+    total = 0,
     lives = 3,
     gameOver = false,
-    audio;
+    audio,
+    loadCredits = false;
 
 var dog = {
     x: 0,
     y : height/2,
     w : 54,
     h : 90,
-    speed : 5,
+    speed : 6,
     img : new Image()
 };
 var cat = {
@@ -33,6 +34,15 @@ var cat = {
     y : 0,
     w : 90,
     h : 90,
+    img : new Image()
+};
+
+var battery = {
+    x: 200,
+    y : 200,
+    w : 40,
+    h : 25,
+    speed : 4,
     img : new Image()
 };
 var rCat = new Image();
@@ -48,44 +58,106 @@ function getMousePos(canvas, event) {
 }
 //Function to check whether a point is inside a rectangle
 function isInside(pos, rect){
-    return pos.x > rect.x && pos.x < rect.x+rect.width && pos.y < rect.y+rect.heigth && pos.y > rect.y
+    return pos.x > rect.x && pos.x < rect.x+rect.width && pos.y < rect.y+rect.height && pos.y > rect.y
 }
 
 //The rectangle should have x,y,width,height properties
-var rect = {
+var go_Button = {
     x:180,
     y:400,
     width:50,
-    heigth:50
+    height:50
+};
+
+var off_Button = {
+    x:20,
+    y:20,
+    width:20,
+    height:20
+};
+
+var on_Button = {
+    x:50,
+    y:20,
+    width:20,
+    height:20
+};
+
+var credit_Button = {
+    x:180,
+    y:440,
+    width:60,
+    height:30
 };
 
 canvas.addEventListener('click', function(evt) {
     var mousePos = getMousePos(canvas, evt);
 
-    if (isInside(mousePos,rect) & lives>0) {
+    if (isInside(mousePos,go_Button) && lives>0) {
         alive=true;
         startTime = Date.now();
-        //audio.pause();
     }
-    if (isInside(mousePos,rect) & lives<=0) {
-        alive=true;
+    if (isInside(mousePos,go_Button) && lives<=0) {
+        alive=false;
         lives = 3;
+        total = 0;
+        startTime = Date.now();
+        console.log('ping');
+    }
+    if (isInside(mousePos,off_Button)) {
+        audio.pause();
+        console.log('pause');
+    }
+    if (isInside(mousePos,on_Button)) {
+        audio.play();
+        console.log('play');
+    }
+
+    if (isInside(mousePos,credit_Button)) {
+        loadCredits = true;
     }
 }, false);
 
+function audioButtons(){
+    ctx.beginPath();
+    ctx.rect(off_Button.x, off_Button.y, off_Button.height, off_Button.width);
+    ctx.fillStyle = '#FFFFFF';
+    ctx.fillStyle = 'rgba(225,225,225,0.5)';
+    ctx.fill();
+    ctx.lineWidth = 2;
+    ctx.strokeStyle = '#000000';
+    ctx.font = "bold 16px Arial";
+    ctx.fillStyle = 'black';
+    ctx.fillText('Music: ', 30, 15, 35);
+    ctx.font = "bold 16px Arial";
+    ctx.fillStyle = 'black';
+    ctx.fillText('Off', 22, 35, 15);
+    ctx.stroke();
+    ctx.closePath();
+    ctx.beginPath();
+    ctx.rect(on_Button.x, on_Button.y, on_Button.height, on_Button.width);
+    ctx.fillStyle = '#FFFFFF';
+    ctx.fillStyle = 'rgba(225,225,225,0.5)';
+    ctx.fill();
+    ctx.lineWidth = 2;
+    ctx.strokeStyle = '#000000';
+    ctx.font = "bold 16px Arial";
+    ctx.fillStyle = 'black';
+    ctx.fillText('On', 52, 35, 15);
+    ctx.stroke();
+    ctx.closePath();
+}
 function startScreen(){
-    var catPosX = 180;
+    var catPosX = 165;
     var catPosY = 180;
     var sec = 10;
-    // audio = new Audio('assets/BrandonMorris_LoadingScreenLoop.wav');
-    // audio.play();
     ctx.drawImage(rCat, 0, 0, cat.w, cat.h, catPosX, catPosY, cat.w, cat.h);
-    ctx.font = "bold 36px Six Caps";
+    ctx.font = "bold 30px Bangers";
     ctx.fillStyle = 'black';
-    ctx.fillText('THE ADVENTURES OF', 25, 300, 200);
-    ctx.font = "bold 36px Bangers";
+    ctx.fillText('THE ADVENTURES OF', 110, 300, 200);
+    ctx.font = "bold 42px Bangers";
     ctx.fillStyle = 'black';
-    ctx.fillText('ROOMBA CAT', 25, 400, 350);
+    ctx.fillText('ROOMBA CAT', 115, 350, 350);
     ctx.beginPath();
     ctx.rect(180, 400, 50, 50);
     ctx.fillStyle = '#FFFFFF';
@@ -98,11 +170,13 @@ function startScreen(){
     ctx.fillText('GO!', 190, 430, 45);
     ctx.stroke();
     ctx.closePath();
+    loadCredits = false;
 }
 
 function createEnemies (){
     for (var i = 0; i < enemyTotal; i++) {
         dog.x += dog.w + Math.random() * (60 - 20) + 20;
+
         if ((dog.x + dog.w)>=width){
             dog.x = Math.random() * 10;
             dog.y = dog.y + 100;
@@ -125,6 +199,40 @@ function createEnemies (){
             });
         }
     }
+}
+function drawEnemies() {
+    for (var i = 0; i < enemies.length; i++) {
+        dog.img.src = 'assets/dog.png';
+        ctx.drawImage(dog.img, frameIndex * dog.w, 0, dog.w, dog.h, enemies[i].x, enemies[i].y,  dog.w, dog.h);
+    }
+}
+
+function moveEnemies() {
+    for (var i = 0; i < enemies.length; i++) {
+        if (enemies[i].y <= height) {
+            enemies[i].y -= enemies[i].speed + (total/5);
+        }
+        if (enemies[i].y < -90) {
+            enemies[i].y = height;
+            enemies[i].x =  Math.random() * width;
+        }
+    }
+    currentTime = Date.now();
+}
+function createLives (){
+
+}
+
+function drawLives() {
+        battery.img.src = 'assets/battery.png';
+        ctx.drawImage(battery.img, 0, 0,battery.w, battery.h, battery.x, battery.y,  battery.w, battery.h);
+        if (battery.y <= height) {
+            battery.y -= battery.speed;
+        }
+        if  (battery.y < -90){
+            battery.y = height;
+            battery.x = Math.random() * width;
+        }
 }
 
 function clearCanvas() {
@@ -188,52 +296,31 @@ function updateCFrame() {
     }
 }
 
-function drawEnemies() {
-    for (var i = 0; i < enemies.length; i++) {
-        dog.img.src = 'assets/dog.png';
-        ctx.drawImage(dog.img, frameIndex * dog.w, 0, dog.w, dog.h, enemies[i].x, enemies[i].y,  dog.w, dog.h);
-    }
-}
-
-function drawFurniture() {
-    for (var i = 0; i < 3; i++) {
-        var furniture = new Image();
-        var fWidth = 100, fHeight = 85, fX, fY = height;
-        fY+=100;
-        fX = Math.random() * 350;
-        furniture.src = 'assets/small_tables.png';
-        ctx.drawImage(furniture, fWidth, 0, fWidth, fHeight, fX, fY, fWidth, fHeight);
-        console.log('Furniture!');
-    }
-    //console.log(enemies.length);
-}
-
-function moveEnemies() {
-    for (var i = 0; i < enemies.length; i++) {
-        if (enemies[i].y <= height) {
-            enemies[i].y += -enemies[i].speed;
-        }
-        if (enemies[i].y < -90) {
-           enemies[i].y = height;
-           enemies[i].x =  Math.random() * width;
-        }
-    }
-    currentTime = Date.now();
-}
-
 function shipCollision() {
-    var cat_xw = cat.x + cat.w,
-        cat_yh = cat.y + cat.h;
     for (var i = 0; i < enemies.length; i++) {
         if(lives > 0){
             if (cat.x < enemies[i].x + enemies[i].w && cat.x + cat.w > enemies[i].x && cat.y < enemies[i].y + enemies[i].h && cat.h + cat.y > enemies[i].y) {
                 enemies[i].y = height;
                 enemies[i].x =  Math.random() * width;
                 lives = lives -1;
-                console.log("###" + i + ": " + enemies[i].y);
             }
         } else {
             alive = false;
+        }
+    }
+}
+
+function collectLife() {
+    if(lives > 0) {
+        if (cat.x < battery.x + battery.w && cat.x + cat.w > battery.x && cat.y < battery.y + battery.h && cat.h + cat.y > battery.y) {
+            battery.y = height;
+            battery.x = Math.random() * width;
+            if (lives <= 5){
+                lives = lives + 1;
+            } else {
+                lives = 5;
+            }
+
         }
     }
 }
@@ -245,7 +332,10 @@ function drawTrimmings() {
         ctx.drawImage(heart, (i*30), 450);
     }
     total = Math.floor((currentTime - startTime)/1000);
-    //console.log(total);
+    ctx.font = "bold 16px Arial";
+    ctx.fillStyle = 'black';
+    ctx.fillText(total, 300, 475);
+
 }
 
 function keyDown(e) {
@@ -263,12 +353,22 @@ function keyUp(e) {
 }
 
 function scoreTotal() {
+    var highscore = localStorage.getItem("highscore");
     if (lives <= 0) {
+        if(highscore === null){
+            localStorage.setItem("highscore", total );
+        }else{
+            if (total > highscore) {
+                localStorage.setItem("highscore", total );
+            }
+        }
         gameOver = true;
-        ctx.font = "bold 36px Arial";
-        ctx.fillText('GAME OVER!', 95, 300);
+        loadCredits = false;
+        ctx.font = "bold 36px Bangers";
+        ctx.fillText('GAME OVER!', 130, 300);
+        ctx.fillText('High Score: '+ highscore, 115, 340);
         ctx.beginPath();
-        ctx.rect(180, 400, 60, 50);
+        ctx.rect(180, 400, 60, 30);
         ctx.fillStyle = '#FFFFFF';
         ctx.fillStyle = 'rgba(225,225,225,0.5)';
         ctx.fill();
@@ -279,35 +379,97 @@ function scoreTotal() {
         ctx.fillText('Menu!', 190, 420, 45);
         ctx.stroke();
         ctx.closePath();
+        ctx.beginPath();
+        ctx.rect(180, 440, 60, 30);
+        ctx.fillStyle = '#FFFFFF';
+        ctx.fillStyle = 'rgba(225,225,225,0.5)';
+        ctx.fill();
+        ctx.lineWidth = 2;
+        ctx.strokeStyle = '#000000';
+        ctx.font = "bold 16px Arial";
+        ctx.fillStyle = 'black';
+        ctx.fillText('Credits', 185, 460, 48);
+        ctx.stroke();
+        ctx.closePath();
     }
+}
+
+function creditScreen(){
+    ctx.fillStyle = 'black';
+    ctx.textAlign = 'center';
+    ctx.font = "bold 20px Bangers";
+    ctx.fillText('Credits', 200, 100);
+    ctx.fillText('Music: ', 200, 130);
+    ctx.font = "16px Arial";
+    ctx.fillText('Cat Song -- Dan Knoflicek', 200, 150);
+    ctx.font = "bold 16px Arial";
+    ctx.fillText('Available at: ', 200, 170);
+    ctx.font = "16px Arial";
+    ctx.fillText('http://opengameart.org/content/cat-song', 200, 190);
+    ctx.font = "20px Bangers";
+    ctx.fillText('Sprites ', 200, 220);
+    ctx.fillText('Monty the Dogs: ', 200, 250);
+    ctx.font = "16px Arial";
+    ctx.fillText('Hiura Flour', 200, 270);
+    ctx.font = "20px Bangers";
+    ctx.fillText('Mac, the Roomba Cat: ', 200, 300);
+    ctx.font = "16px Arial";
+    ctx.fillText('Based on content by Hiura Flour', 200, 320);
+    ctx.font = "bold 16px Arial";
+    ctx.fillText('Spritesheet available at: ', 200, 340);
+    ctx.font = "16px Arial";
+    ctx.fillText('https://grandmadebslittlebits.wordpress.com/', 200, 360, 290);
+    ctx.font = "20px Bangers";
+    ctx.fillText('Background: ', 200, 390);
+    ctx.font = "16px Arial";
+    ctx.fillText('Tiziana', 200, 410);
+    ctx.font = "bold 16px Arial";
+    ctx.fillText('Available at: ', 200, 430);
+    ctx.font = "16px Arial";
+    ctx.fillText('http://opengameart.org/content/terracottabeige-stone-tile-floor-256px', 200, 450, 320);
+    ctx.font = "20px Bangers";
+    ctx.fillText('Battery Sprite & Code : Aisling Guinan', 200, 470);
+    ctx.font = "10px Bangers";
+    ctx.fillText('4BCT -- 13364696 -- 2016', 200, 490);
 }
 
 function init() {
     createEnemies();
     setInterval(gameLoop, 25);
-    setInterval(updateDFrame, 250);
+    setInterval(updateDFrame, 250 + (total/5));
     setInterval(updateCFrame, 2000);
     document.addEventListener('keydown', keyDown, false);
     document.addEventListener('keyup', keyUp, false);
+    audio = new Audio('assets/Cat_Song_Dan_Knoflicek.wav');
+    audio.addEventListener('canplaythrough', function() {
+        this.play();
+        this.loop = true;
+    }, false);
 }
 
 function gameLoop() {
     clearCanvas();
+    audioButtons();
     if(alive){
         drawCat();
         drawEnemies();
         moveEnemies();
-        drawFurniture();
+        createLives();
+        drawLives();
+        collectLife();
         shipCollision();
         drawTrimmings();
-        document.getElementById('sound').pause();
-
     }
-    if(!alive && lives>0){
+    if(!alive && lives>0 && loadCredits==false){
         startScreen();
-        document.getElementById('sound').play();
     }
-    scoreTotal();
+    if(!alive && loadCredits==true){
+        creditScreen();
+    }
+    if(loadCredits==false){
+        scoreTotal();
+    }
+
 }
 
     window.onload = init;
